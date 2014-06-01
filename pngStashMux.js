b8c7a317e7bd8a761ac30752c028c40b414e7a81
Helge Skogly Holm <helge.holm@gmail.com>
2014-06-01 21:24:01 +0200

    Added read and write.

--- pngStashMux.js
+++ pngStashMux.js
@@ -46,14 +46,83 @@ module.exports = function pngStashMux(files, cb) {
   function set(idx, val, cb) {
     openFileFor(idx, function(err) {
       if (err) return cb(err);
-      var fileOffset = index.files[currentOpenPng.idx].pos;
-      currentOpenPng.stash.setByte(idx - fileOffset, val);
+      var filePos = index.files[currentOpenPng.idx].pos;
+      currentOpenPng.stash.setByte(idx - filePos, val);
       currentOpenPng.synced = false;
       cb(null, val);
     });
   }
 
+  function read(offset, length, cb) {
+    if (length === undefined) { // read(cb)
+      cb = offset;
+      offset = 0;
+      length = index.length; // read(offset, cb)
+    }
+    if (cb === undefined) {
+      cb = length;
+      length = index.length - offset;
+    }
+      
+    if (offset + length > index.length)
+      return cb("Read would go out of bounds: " +
+                offset + " + " + length + " > " + index.length);
+
+    openFileFor(offset, function(err) {
+      if (err) return cb(err);
+      var filePos = index.files[currentOpenPng.idx].pos;
+      var fileLength = currentOpenPng.stash.length;
+      var fileOffset = offset - filePos;
+      var fileReadLength = length;
+      if (fileReadLength + fileOffset > fileLength)
+        fileReadLength = fileLength - fileOffset;
+      var data = currentOpenPng.stash.read(fileOffset, fileReadLength);
+      if (fileReadLength == length)
+        return cb(null, data);
+      read(offset + fileReadLength, length - fileReadLength, function(err, remData) {
+        cb(null, Buffer.concat([data, remData]));
+      });
+    });
+  }
+
+  function write(writeData, offset, length, cb) {
+    if (length === undefined) { // write(data, cb)
+      cb = offset;
+      offset = 0;
+      length = writeData.length;
+    }
+    if (cb === undefined) { // write(data, offset, cb)
+      cb = length;
+      length = writeData.length;
+    }
+
+    if (offset + length > index.length)
+      return cb("Write would go out of bounds: " +
+                offset + " + " + length + " > " + index.length);
+
+    if (length == 0)
+      return cb();
+
+    openFileFor(offset, function(err) {
+      if (err) return cb(err);
+      var filePos = index.files[currentOpenPng.idx].pos;
+      var fileLength = currentOpenPng.stash.length;
+      var fileOffset = offset - filePos;
+      var fileWriteLength = length;
+      if (fileWriteLength + fileOffset > fileLength)
+        fileWriteLength = fileLength - fileOffset;
+      currentOpenPng.stash.write(writeData, fileOffset, fileWriteLength);
+      currentOpenPng.synced = false;
+      if (fileWriteLength == length)
+        return cb();
+      var remainingData = writeData.slice(fileWriteLength);
+      write(remainingData, offset + fileWriteLength, length - fileWriteLength, cb);
+    });
+  }
+  
   functions = {
+    write: write,
+    read: read,
     getByte: get,
     setByte: set,
     save: save
